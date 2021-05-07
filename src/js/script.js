f5e27ab05c3f9e8ecd61442284ecc30326476500
Kamil Put <kamil.put@interia.pl>
2021-05-07 13:00:04 +0200

    Add function to filter books, add color to ratings

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,8 @@
     },
     all: {
       bookLink: '.book__image',
-      bookId: 'data-id'
+      bookId: 'data-id',
+      form: '.filters',
     },
   };
   const classNames = {
@@ -24,7 +25,20 @@
   function render(){
 
     for(let book of dataSource.books){
-      const generatedHTML = templates.books(book);
+
+      const ratingBgc = determineRatingBgc(book.rating);
+      const ratingWidth = book.rating * 10;
+      //const generatedHTML = templates.books(book);
+      const generatedHTML = templates.books({
+        id: book.id,
+        name: book.name,
+        price: book.price,
+        image: book.image,
+        rating: book.rating,
+        ratingBgc,
+        ratingWidth,
+      });
+
       const element = utils.createDOMFromHTML(generatedHTML);
       const booksContainer = document.querySelector(select.containerOf.list);
       booksContainer.appendChild(element);
@@ -52,7 +66,62 @@
         console.log('Favorite books:', favoriteBooks);
       }
     });
+    const formElem = document.querySelector(select.all.form);
+
+    formElem.addEventListener('change', function(event){
+      event.preventDefault();
+      const clickedElem = event.target;
+      if(clickedElem.tagName == 'INPUT' && clickedElem.type == 'checkbox' && clickedElem.name == 'filter'){
+        console.log(clickedElem.value);
+        if(clickedElem.checked == true){
+          filters.push(clickedElem.value);
+          console.log('Filters:', filters);
+        } else{
+          const indexOfFilters = filters.indexOf(clickedElem.value);
+          filters.splice(indexOfFilters, 1);
+          console.log('Filters:', filters);
+        }
+      }
+      filterBooks();
+    });
   }
+
+  let filters = [];
+
+  function filterBooks(){
+
+    for(let book of dataSource.books){
+      let shouldBeHidden = false;
+      for(let filter of filters){
+        if(!book.details[filter]){
+          shouldBeHidden = true;
+          break;
+        }
+      }
+      const findBook = document.querySelector('.book__image[data-id="' + book.id + '"]');
+      if(shouldBeHidden == true){
+        findBook.classList.add('hidden');
+      } else{
+        findBook.classList.remove('hidden');
+      }
+    }
+  }
+
+  function determineRatingBgc(rating){
+
+    let background = '';
+    if(rating < 6){
+      background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    } else if(rating > 6 && rating <= 8){
+      background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if(rating > 8 && rating <= 9){
+      background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if(rating > 9){
+      background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+    return background;
+  }
+
 
   render();
   initActions();
